77feb18b77de34d46e5f67ede92a5fae05998ee0
Zasper <nicolaszasper@gmail.com>
2025-06-11 14:41:20 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando Nokia:");
+            Smartphone nokia = new Nokia("11999999999", "Nokia 3310", "123456789012345", 64);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("Whasapp");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Testando iPhone:");
+            Smartphone iphone = new Iphone("11988888888", "iPhone 14", "543210987654321", 256);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
